b1c999d078ce89a1ba43554e97725c605744441e
Lars Ingebrigtsen <larsi@gnus.org>
2024-08-12 14:28:28 +0200

    Make things work with textareas

--- pgdn-fixer.js
+++ pgdn-fixer.js
@@ -4,20 +4,31 @@
 // @grant    none
 // ==/UserScript==
 
-Array.from(document.querySelectorAll("input")).forEach(function(input) {
+function pgdnScroller(e, input) {
+  if (e.keyCode == 38)
+    window.scrollBy(0, -10);
+  else if (e.keyCode == 40)
+    window.scrollBy(0, 10);
+  else if (e.keyCode == 33)
+    window.scrollBy(0, -window.innerHeight);
+  else if (e.keyCode == 34) 
+    window.scrollBy(0, window.innerHeight);
+  else
+    return true;
+  e.preventDefault();
+  input.blur();
+  return false;
+}
+
+Array.from(document.querySelectorAll("input,textarea")).forEach(function(input) {
   input.addEventListener("keydown", function(e) {
-    if (e.keyCode == 38)
-      window.scrollBy(0, -10);
-    else if (e.keyCode == 40)
-      window.scrollBy(0, 10);
-    else if (e.keyCode == 33)
-      window.scrollBy(0, -window.innerHeight);
-    else if (e.keyCode == 34) 
-      window.scrollBy(0, window.innerHeight);
-    else
-      return true;
-    e.preventDefault();
-    input.blur();
-    return false;
+    pgdnScroller(e, input);
   });
+});
+
+document.addEventListener("keydown", function(e) {
+  var name = document.activeElement.nodeName;
+  if (name == "INPUT" || name == "TEXTAREA") {
+    pgdnScroller(e, document.activeElement);
+  };
 });
